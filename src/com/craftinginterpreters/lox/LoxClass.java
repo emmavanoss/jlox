c57b06c3662177235d19c4212b02168c4928af60
Emma Van Oss <e.vanoss@gmail.com>
2020-04-15 16:22:20 +0100

    classes have class methods

--- src/com/craftinginterpreters/lox/LoxClass.java
+++ src/com/craftinginterpreters/lox/LoxClass.java
@@ -5,9 +5,19 @@ import java.util.Map;
 
 public class LoxClass implements LoxCallable {
   final String name;
+  final Map<String, LoxFunction> methods;
 
-  LoxClass(String name) {
+  LoxClass(String name, Map<String, LoxFunction> methods) {
     this.name = name;
+    this.methods = methods;
+  }
+
+  public LoxFunction findMethod (String name) {
+    if (methods.containsKey(name)) {
+      return methods.get(name);
+    }
+
+    return null;
   }
 
   @Override
